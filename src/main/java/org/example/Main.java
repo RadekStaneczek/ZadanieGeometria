dbf51c4b84a6a60e9e11567c06e8e3063c528221
RadekStaneczek <radek.staneczek@gmail.com>
2026-04-20 10:41:30 +0200

    Library basic example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args)
-    {
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        double[] Px = new double[]{1.0,7.0,7.0,1.0,1.0};
+        double[] Py = new double[]{1.0,1.0,5.0,5.0,1.0};
+
+        XYChart chart = QuickChart.getChart("Punkty", "X", "Y", ".", Px, Py);
+
+        chart.addSeries("Trojkat",new int[]{15,40,30,15},new int[]{5,10,20,5});
+        new SwingWrapper(chart).displayChart();
     }
 }
